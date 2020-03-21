733b2205423765897afdcb0760ef21ee653edc58
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2020-03-21 19:43:07 +0200

    Create basic-usage.js

---
+++ examples/basic-usage.js
@@ -0,0 +1,16 @@
+const Searxuser = require('searx-api');
+
+const searx = new Searxuser(
+  'https://searx.asdew.kaikkitietokoneista.net/', //Url
+  'https', //Protocol
+  'en-US', //Language
+  '1' //Safe search
+)
+
+searx.find("kaikkitietokoneista", function(data) {
+  console.log("Searching for: " + data.query + "\n\n");
+
+  data.results.forEach((item) => {
+    console.log("TITLE: " + item.title + "\n\t URL: " + item.url);
+  });
+});
